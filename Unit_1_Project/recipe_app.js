415280480afa386417a1fb9de400291d995636ba
ccosat1isd <ccosat1isd@ccosat08isd.local>
2019-04-16 22:22:14 -0600

    Added CORS Error fix to app.js

--- Unit_1_Project/recipe_app.js
+++ Unit_1_Project/recipe_app.js
@@ -1,30 +1,21 @@
 $(() => {
-  $('form').on('submit', (event) => {
-    event.preventDefault();
-    const userInput = $('input[type="text"]').val();
-//     $.ajax(
-//     {
-//       url:'http://www.recipepuppy.com/api/',
-//       data: {
-//       "$limit" : userInput
-//       }
-//     }
-//     ).then(
-//       (data) => {
-//         $('#police').html(data)
-//         for (let i =0; i < data.length; i ++){
-//           $('#complaint').append(data[i].complaint_type)
-//           $('#res').append(data[i].resolution_action_updated_date)
-//         }
-//         console.log(data);
-//       }
-//     )
-//   })
-// })
+  // $('form').on('submit', (event) => {
+  //   event.preventDefault();
+  //
+  //   const userInput = $('input[type="text"]');
+  $.ajax({
+    url: 'http://www.recipepuppy.com/api/',
+    dataType: "jsonp",
+    jsonpCallback: "logResults",
+    success: (data) => {
+      console.log(data);
+    }
+  });
+})
 
 // Form for ingredient entry and submit button needed
 // When ingredients are entered a modal should pop up with the recipes
     // modal will need a close button and a scroll bar
 
 // Design needs to be responsive
-// Phone and tablet designs will need a different main font
+// Note: Phone and tablet designs will need a different main font
